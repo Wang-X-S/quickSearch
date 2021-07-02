d586c3776c2df89ad4f9f26c2378e9fe0a211991
13jun <820373584@qq.com>
2021-07-02 22:12:39 +0800

    实现保存到本地 localStorage

--- src/main.js
+++ src/main.js
@@ -1,24 +1,83 @@
-$('.addButton').on('click',()=>{
+const $siteList = $('.siteList')
+const $addWeb = $('.addWeb')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject|| [
+    { logo:'aliyun', logoType:'svgSelf',url:'https://www.aliyun.com/'},
+    { logo:'bilibili', logoType:'svgSelf',url:'https://www.bilibili.com/'},
+    { logo:'cndblogs', logoType:'svgSelf',url:'https://www.cnblogs.com/'},
+    { logo:'GitHub', logoType:'svgSelf',url:'https://github.com/'},
+    { logo:'Stackoverflow', logoType:'svgSelf',url:'https://stackoverflow.com/'},
+    { logo:'Leetcode', logoType:'svgSelf',url:'https://leetcode-cn.com/'},
+    { logo:'zhihu', logoType:'svgSelf',url:'https://www.zhihu.com/'},
+
+]
+
+const render =()=>{hashMap.forEach(node=>{
+    if(node.logoType==='svgSelf'){
+    let $liSelf = $(`
+            <li>
+                <a href="${node.url}">
+                    <div class="site">
+                        <div class="logo">
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-${(node.logo).toLowerCase()}"></use>
+                            </svg>
+                        </div>
+                        <dig class="link">${node.logo}</dig>
+                    </div>
+                </a>
+            </li>
+    `).insertBefore($addWeb)
+    }else{
+        let $liSelf = $(`
+            <li>
+                <a href="${node.url}">
+                    <div class="site">
+                        <div class="logo">
+                            ${node.logo[0]}
+                        </div>
+                        <dig class="link">${node.logo}</dig>
+                    </div>
+                </a>
+            </li>
+    `).insertBefore($addWeb)
+    }
+})
+}
+render()
+$('.addButton').on('click', () => {
+    $siteList.find('li:not(.addWeb)').remove()
     console.log('123')
     let url = window.prompt(
         '请输入你要添加的网址'
     )
 
-    if(url.indexOf('http')!==0){
-            url = 'https://' + url
+    if (url.indexOf('http') !== 0) {
+        url = 'https://' + url
     }
     console.log(url)
-    const $siteList = $('.siteList')
-    const $addWeb = $('.addWeb')
-    const $li = $(`
-            <a href="${url}">
-                <li class="last">
-                    <div class="site">
-                        <div class="logo">
-                            ${url[0]}
-                        </div>
-                        <dig class="link">${url}</dig>
-                    </div>
-                </li>
-           </a>`).insertBefore('.addWeb')
+
+    hashMap.push({
+        logo: url[0],
+        logoType:'text',
+        url:url
+    })
+
+    render()
+    // const $li = $(`
+    //           <li>       
+    //               <a href="${url}">
+    //                 <div class="site">
+    //                     <div class="logo">
+    //                         ${url[0]}
+    //                     </div>
+    //                     <dig class="link">${url}</dig>
+    //                 </div>     
+    //              </a>
+    //          </li>`).insertBefore('.addWeb')
 })
+window.onbeforeunload =()=>{
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x',string)
+}
